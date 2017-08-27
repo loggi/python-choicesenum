8ccd1a267786e31b7c6155f42af19bb4a583d8dd
Fernando Macedo <fgmacedo@gmail.com>
2017-08-27 20:56:57 -0300

    Travis setup

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'loggi/choicesenum'
+GITHUB_REPO = 'loggi/python-choicesenum'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
