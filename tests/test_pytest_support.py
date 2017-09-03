3e5bd2ad293e7846be534239a4403b7826b56e77
Fernando Macedo <fgmacedo@gmail.com>
2017-09-03 20:55:25 -0300

    Fix pep-8

--- tests/test_pytest_support.py
+++ tests/test_pytest_support.py
@@ -5,14 +5,14 @@ def test_pytest_make_parametrize_id(testdir):
     testdir.makepyfile("""
         import pytest
         from choicesenum import ChoicesEnum
-        
+
         @pytest.fixture
         def django_test_environment():
             # To hide the error:
             "RuntimeError: setup_test_environment() was already called and" 
             "can't be called again without first calling teardown_test_environment()"
             pass
-        
+
         class Color(ChoicesEnum):
             RED = '#f00', 'Vermelho'
             GREEN = '#0f0', 'Verde'
