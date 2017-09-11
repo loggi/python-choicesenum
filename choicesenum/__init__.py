0725634183d970b605679582e80200c96a486fdf
Fernando Macedo <fgmacedo@gmail.com>
2017-09-11 01:42:42 -0300

    Bump version: 0.1.7 → 0.2.0

--- choicesenum/__init__.py
+++ choicesenum/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 __author__ = """Fernando Macedo"""
 __email__ = 'fgmacedo@gmail.com'
-__version__ = '0.1.7'
+__version__ = '0.2.0'
 
 __all__ = ('ChoicesEnum', )
 
