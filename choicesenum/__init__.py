ab5e1a15ffb0bad900fb0ccaabfff9a7fe949c73
Fernando Macedo <fgmacedo@gmail.com>
2017-12-01 12:12:43 -0200

    Bump version: 0.2.1 → 0.2.2

--- choicesenum/__init__.py
+++ choicesenum/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 __author__ = """Fernando Macedo"""
 __email__ = 'fgmacedo@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __all__ = ('ChoicesEnum', )
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup_requirements = [
 
 setup(
     name='choicesenum',
-    version='0.2.1',
+    version='0.2.2',
     description="Python's Enum with extra powers to play nice with labels and choices fields",
     long_description=readme + '\n\n' + history,
     author="Fernando Macedo",
