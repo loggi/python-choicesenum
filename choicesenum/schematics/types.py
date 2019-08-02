443b98edc21181a93a8653e983b550a9a57c9501
Maiky Guanaes <maiky.guanaes@loggi.com>
2019-08-02 16:27:28 -0300

    feat(schematics) Creates ChoicesEnumType

---
+++ choicesenum/schematics/types.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from __future__ import absolute_import, unicode_literals
+
+from choicesenum import ChoicesEnum
+from schematics.types import BaseType, ValidationError
+
+
+class ChoicesEnumType(BaseType):
+    def __init__(self, type_):
+        if not issubclass(type_, ChoicesEnum):
+            raise ValidationError(self.Messages.INVALID_TYPE)
+        super(ChoicesEnumType, self).__init__(type_)
+        self.type = type_
+
+    def to_native(self, value, context=None):
+        return self.type(value)
+
+    def to_primitive(self, value, context=None):
+        return self.type(value).value
+
+    class Messages:
+        INVALID_TYPE = 'Expected a ChoicesEnum sub type'
